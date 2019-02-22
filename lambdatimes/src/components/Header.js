8031d4ecab1ad8348176d051aaee37c4681dd00d
ArthurLeonard <ialeonard@ucdavis.edu>
2019-02-22 15:12:33 -0800

    mvp

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,79 +1,21 @@
 import React from "react";
 import styled, { css } from "styled-components";
 import { Alert } from "reactstrap";
+// import styled from 'styled-components'
 
 // Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
 
-// .header {
-//   display: flex;
-//   justify - content: space - between;
-//   align - items: center;
-//   flex - direction: row;
-//   background - color: #fff;
-//   border - bottom: 1px solid lightgrey;
-//   width: 100 %;
-//   height: 120px;
-//   margin - top: 44px;
-//   padding - bottom: 15px;
-// }
-// @media(min - width: 1280px) {
-//   .header {
-//     width: 1280px;
-//   }
-// }
-// .header h1 {
-//   font - size: 60px;
-//   font - family: Didot, serif;
-//   font - weight: bold;
-//   flex: 8;
-//   text - align: center;
-//   color: #000;
-// }
-// .header span {
-//   align - self: flex - end;
-//   font - size: 11px;
-//   font - weight: bold;
-//   letter - spacing: 1px;
-// }
-// .header.date {
-//   margin - left: 25px;
-//   flex: 1;
-// }
-// .header.temp {
-//   text - align: right;
-//   margin - right: 25px;
-//   flex: 1;
-// }
-
-// .header {
-//   display: flex;
-//   justify - content: space - between;
-//   align - items: center;
-//   flex - direction: row;
-//   background - color: #fff;
-//   border - bottom: 1px solid lightgrey;
-//   width: 100 %;
-//   height: 120px;
-//   margin - top: 44px;
-//   padding - bottom: 15px;
-// }
-export const FlexWrapper = styled.div`
-  display: flex;
-  ${props =>
-    props &&
-    css`
-      line-height: 8px;
-    `};
-`;
+const StyledDiv = styled.div``;
+const StyledHeading = styled.h1``;
+const StyledSpan = styled.span``;
 
 const Header = () => {
   return (
     <div className="header">
-      <Alert color="green">This is a primary alert — check it out!</Alert>
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
+      <StyledSpan>SMARCH 32, 2018</StyledSpan>
+      <StyledHeading>Lambda Times</StyledHeading>
+      <StyledSpan>98°</StyledSpan>
     </div>
   );
 };
